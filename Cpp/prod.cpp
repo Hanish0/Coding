151179b8fe60c52676d922ed71f8db08c0f473c8
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-03-10 11:12:01 +0500

    Update prod.cpp

--- Cpp/prod.cpp
+++ Cpp/prod.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 int main() {
   int a, b;
-  cout << "ENter a: "
+  cout << "Enter a: ";
+  cin >> a;
+  cout << "Enter b: ";
+  cin >> b;
+  int prod = a * b;
+  cout << "Product is: " << prod << endl;
+  return 0;
+}
+    
