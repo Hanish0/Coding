4cf78e6bece6324972337fa9b5faf7096ac339c2
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-03-19 09:35:50 +0500

    Update list1.py

--- Pyhton/List/list1.py
+++ Pyhton/List/list1.py
@@ -2,10 +2,9 @@
 
 # Take input in a list
 
-a = []
-for i in range(0, 10):
+a = []                                # initialize an empty list
+for i in range(0, 10):                # length of the list is 10
   l = int(input("Enter the value: "))
-  a.append(l);
+  a.append(l);                        # put the values in the list
   
-print("The list is: ", a)
-  
+print("The list is: ", a)             # print the list
