f997c9bbdf7c9907cf8b3fbd65ee7438c7a47515
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2023-03-23 09:51:57 +0500

    Create intro.py

---
+++ Pyhton/Conditional Statements/intro.py
@@ -0,0 +1,16 @@
+# Code here
+
+# We have 2 numbers a and b from the user. If a is greater, print "a is greater" else "b is greater"
+
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+print("Values are: ", a, " ", b)
+
+if a > b:
+  print("a i greater.")
+else:
+  print("b is greater")
+  
+# Program ends here
